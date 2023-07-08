3d0458a31dd8e81502f513188c04bb2c097966d2
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2023-07-08 23:07:23 +0700

    membuat widgets rumahsakittile

---
+++ lib/widgets/rumahsakittile.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:medcare/model/rumahsakit_model.dart';
+import 'package:medcare/theme.dart';
+
+class RumahSakitTile extends StatelessWidget {
+  final RumahSakitModel? rumahSakitModel;
+  const RumahSakitTile(this.rumahSakitModel);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(bottom: 12),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        //  width: SizeConfig.screenWidth * 0.78,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: _getBGClr(rumahSakitModel?.color ?? 0),
+        ),
+        child: Row(children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  rumahSakitModel?.poli ?? "",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.access_time_rounded,
+                      color: Colors.grey[200],
+                      size: 18,
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      "${rumahSakitModel!.startTime} - ${rumahSakitModel!.endTime}",
+                      style: GoogleFonts.lato(
+                        textStyle:
+                            TextStyle(fontSize: 13, color: Colors.grey[100]),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12),
+                Text(
+                  rumahSakitModel?.note ?? "",
+                  style: GoogleFonts.lato(
+                    textStyle: TextStyle(fontSize: 15, color: Colors.grey[100]),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            height: 60,
+            width: 0.5,
+            color: Colors.grey[200]!.withOpacity(0.7),
+          ),
+          RotatedBox(
+            quarterTurns: 3,
+            child: Text(
+              rumahSakitModel!.isCompleted == 1 ? "COMPLETED" : "OBAT",
+              style: GoogleFonts.lato(
+                textStyle: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  _getBGClr(int no) {
+    switch (no) {
+      case 0:
+        return bluishClr;
+      case 1:
+        return pinkClr;
+      case 2:
+        return yellowClr;
+      default:
+        return bluishClr;
+    }
+  }
+}
